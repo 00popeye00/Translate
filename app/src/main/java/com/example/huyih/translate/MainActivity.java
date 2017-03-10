5b13cb39e9de85321e9e2ebe4750b5ee5c5bc0bd
00popeye00 <172136601@qq.com>
2017-03-10 14:30:22 +0800

    New

--- app/src/main/java/com/example/huyih/translate/MainActivity.java
+++ app/src/main/java/com/example/huyih/translate/MainActivity.java
@@ -7,19 +7,25 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText et;
     private TextView tv;
+    private TextView key;
+    private TextView us;
+    private TextView uk;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,55 +34,72 @@ public class MainActivity extends AppCompatActivity {
 
         et= (EditText) findViewById(R.id.editText);
         tv= (TextView) findViewById(R.id.textView1);
+        key= (TextView) findViewById(R.id.key);
+        us= (TextView) findViewById(R.id.us);
+        uk= (TextView) findViewById(R.id.uk);
 
         findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                readNet();
+                readNet("http://fanyi.youdao.com/openapi.do?keyfrom=EToCpath&key=478871880&type=data&doctype=json&version=1.1&q="+et.getText());
             }
         });
     }
 
-    private void readNet() {
+    private void readNet(String url) {
 
-        new AsyncTask<Void,Void,String>(){
+        new AsyncTask<Object,Object,String>(){
 
             @Override
-            protected String doInBackground(Void... voids) {
+            protected String doInBackground(Object... objects) {
 
                 try {
-                    BufferedReader reader=new BufferedReader(new InputStreamReader(new URL("http://fanyi.youdao.com/openapi.do?keyfrom=EToCpath&key=478871880&type=data&doctype=json&version=1.1&q="+et.getText()).openStream(),"utf-8"));
-                    String line=null;
-                    StringBuffer content=new StringBuffer();
+                    URL url=new URL((String) objects[0]);
+                    HttpURLConnection connection= (HttpURLConnection) url.openConnection();
+                    InputStream is =connection.getInputStream();
+                    BufferedReader reader=new BufferedReader(new InputStreamReader(is,"utf-8"));
+                    String line;
+                    StringBuffer builder=new StringBuffer();
 
                     while ((line=reader.readLine())!=null){
-                        content.append(line);
+                        builder.append(line);
                     }
                     reader.close();
-                    return content.toString();
-
+                    return builder.toString();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 return null;
             }
-
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
 
                 if(s!=null){
-                    try {
-                        JSONArray josnArr=new JSONArray(s);
-                        JSONObject
 
+                    try {
+                        JSONObject json=new JSONObject(s);
+//                        解析数组
+//                        JSONArray jsonArray=json.getJSONArray("basic");
+                        JSONObject basic=json.getJSONObject("basic");
+
+//                        System.out.println("英："+basic.getString("us-phonetic"));
+//                        System.out.println("翻译的词："+json.getString("query"));
+//                        System.out.println("翻译："+json.getString("translation"));
+
+                        key.setText(json.getString("query"));
+                        us.setText("美："+basic.getString("us-phonetic"));
+                        uk.setText("英："+basic.getString("uk-phonetic"));
+                        tv.setText(json.getString("translation"));
 
                     } catch (JSONException e) {
                         e.printStackTrace();
                     }
                 }
             }
-        }.execute();
+        }.execute(url);
     }
 
 
